ae131d0a5a5c3994ae4ba365f6cfd2cbc86b2b8f
shrekl1234 <shrekl1234@users.noreply.github.com>
2020-08-31 09:39:46 +0530

    Updates

---
+++ precache-manifest.35025fe8d194227d6b7b3e318e593b5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535fb62b3d697f3cd6c9ed78cf691af3",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "36abc86bf1026eaf2423",
+    "url": "/crwn-clothing/static/css/main.90945d79.chunk.css"
+  },
+  {
+    "revision": "125aef724bcbb46bd382",
+    "url": "/crwn-clothing/static/js/2.50b07e23.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/crwn-clothing/static/js/2.50b07e23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36abc86bf1026eaf2423",
+    "url": "/crwn-clothing/static/js/main.185af07a.chunk.js"
+  },
+  {
+    "revision": "0c0bf0a54c9e5596672f",
+    "url": "/crwn-clothing/static/js/runtime-main.5ffcf0f1.js"
+  },
+  {
+    "revision": "e0f7d09962efb47702a1cf1a6a08a59d",
+    "url": "/crwn-clothing/static/media/logo.e0f7d099.svg"
+  }
+]);
